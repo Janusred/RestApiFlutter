0a6e303957e25bb26bac80141aed0e43c7cc2ee6
Janusred <horaciok300@hotmail.com>
2022-10-19 03:31:35 -0500

    db

---
+++ vacantesI.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: vacantes_db
+-- ------------------------------------------------------
+-- Server version	8.0.27-0ubuntu0.21.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `vacante`
+--
+
+DROP TABLE IF EXISTS `vacante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vacante` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(180) DEFAULT NULL,
+  `descricion` varchar(255) DEFAULT NULL,
+  `empresa` varchar(180) DEFAULT NULL,
+  `telefono` varchar(180) DEFAULT NULL,
+  `correo` varchar(180) DEFAULT NULL,
+  `direccion` varchar(180) DEFAULT NULL,
+  `nombrerh` varchar(180) DEFAULT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vacante`
+--
+
+LOCK TABLES `vacante` WRITE;
+/*!40000 ALTER TABLE `vacante` DISABLE KEYS */;
+INSERT INTO `vacante` VALUES (1,'programador','Flutter','ATX','2411385447','redlds17@hotmail.com','Apizaco','Janus',NULL,'2022-10-06 07:45:21'),(2,'programador','JavaScript','IT-global','2416885056','horaciok300@hotmail.com','Apizaco','lds','https://m.media-amazon.com/images/I/A1dlZfAxfcL._AC_SL1500_.jpg','2022-10-06 07:49:37');
+/*!40000 ALTER TABLE `vacante` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-19  3:31:02
